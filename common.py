e40eaee96f0e0045614cd5872f0a5ec52d5cad99
ciyuan53 <haoyuan.t@outlook.com>
2022-06-16 17:46:27 -0700

    conver most to jupyter

--- common.py
+++ common.py
@@ -1,9 +1,8 @@
-from typing import Iterator, Tuple
-
 import torch
 from torch import nn
 from torch.utils import data
 from torchvision import transforms, datasets
+from typing import Iterator, Tuple
 
 
 def synthetic_data(w: torch.Tensor, b: torch.Tensor, num_examples: int):
@@ -14,8 +13,9 @@ def synthetic_data(w: torch.Tensor, b: torch.Tensor, num_examples: int):
     return X, y
 
 
-def load_data_arrays(data_arrays: Tuple[torch.Tensor, ...], batch_size: int,
-                     shuffle=True):
+def load_data_arrays(
+    data_arrays: Tuple[torch.Tensor, ...], batch_size: int, shuffle=True
+):
     dataset = data.TensorDataset(*data_arrays)
     return data.DataLoader(dataset, batch_size, shuffle)
 
@@ -27,11 +27,15 @@ def load_data_from_mnist(batch_size=200, resize: int = None):
     trans = transforms.Compose(trans)
 
     mnist_train = datasets.FashionMNIST(
-        "/Users/sunny/Desktop/D2L/data.nosync", True, trans, download=True)
+        "/Users/sunny/Desktop/D2L/data.nosync", True, trans, download=True
+    )
     mnist_test = datasets.FashionMNIST(
-        "/Users/sunny/Desktop/D2L/data.nosync", False, trans, download=True)
-    return (data.DataLoader(mnist_train, batch_size, True),
-            data.DataLoader(mnist_test, batch_size, False))
+        "/Users/sunny/Desktop/D2L/data.nosync", False, trans, download=True
+    )
+    return (
+        data.DataLoader(mnist_train, batch_size, True),
+        data.DataLoader(mnist_test, batch_size, False),
+    )
 
 
 def init_weights(m: nn.Module):
@@ -54,12 +58,19 @@ class Accumulator:
         return self.data[idx]
 
 
-def train_ch3(net: nn.Sequential, loss: nn.Module, optimizer: torch.optim.Optimizer,
-              num_epochs: int, train_iter: Iterator, test_iter: Iterator):
-              
+def train_ch3(
+    net: nn.Sequential,
+    loss: nn.Module,
+    optimizer: torch.optim.Optimizer,
+    num_epochs: int,
+    train_iter: Iterator,
+    test_iter: Iterator,
+):
     def count_correct(y_hat: torch.Tensor, y: torch.Tensor):
         if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
-            y_hat = y_hat.argmax(1)  # convert into index of the largest element on dim 1
+            y_hat = y_hat.argmax(
+                1
+            )  # convert into index of the largest element on dim 1
         cmp = y_hat.type(y.dtype) == y  # convert into the same type to avoid weird bug
         return cmp.type(y.dtype).sum().item()
 
@@ -76,7 +87,9 @@ def train_ch3(net: nn.Sequential, loss: nn.Module, optimizer: torch.optim.Optimi
 
         train_loss = metric[0] / metric[2]
         train_acc = metric[1] / metric[2]
-        print(f"epoch {epoch + 1}: loss {train_loss:.8f}, accuracy {100 * train_acc:.2f}%")
+        print(
+            f"epoch {epoch + 1}: loss {train_loss:.8f}, accuracy {100 * train_acc:.2f}%"
+        )
         # assert train_loss < 0.5, train_loss
         # assert 0.7 < train_acc <= 1.0, train_acc
 
@@ -87,5 +100,5 @@ def train_ch3(net: nn.Sequential, loss: nn.Module, optimizer: torch.optim.Optimi
             metric.add(count_correct(net(X), y), y.numel())
 
     test_acc = metric[0] / metric[1]
-    print(f"test dataset: accuracy {100 * test_acc:.2f}%")
+    print(f"\ntest dataset: accuracy {100 * test_acc:.2f}%")
     # assert 0.7 < test_acc <= 1.0, test_acc
