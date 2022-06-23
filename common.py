a1af476cbaa39844c18bd88d8b5652cf4fe323c5
ciyuan53 <haoyuan.t@outlook.com>
2022-06-23 11:29:53 -0700

    add m1 gpu support

--- common.py
+++ common.py
@@ -1,9 +1,8 @@
-from tokenize import Number
 import torch
-from torch import Tensor, nn
+from torch import nn
 from torch.utils import data
 from torchvision import transforms, datasets
-from typing import Iterator, Tuple
+from typing import Iterator, Tuple, Literal
 
 
 def synthetic_data(w: torch.Tensor, b: torch.Tensor, num_examples: int):
@@ -60,18 +59,37 @@ def count_correct(y_hat: torch.Tensor, y: torch.Tensor):
     return cmp.type(y.dtype).sum().item()
 
 
-def train_ch3(
+def init_weights_ch3(m: nn.Module):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, 0, 0.01)
+
+
+def init_weights_ch6(m: nn.Module):
+    type_module = type(m)
+    if type_module == nn.Linear or type_module == nn.Conv2d:
+        nn.init.xavier_uniform_(m.weight)
+
+
+def train(
     net: nn.Sequential,
     train_iter: Iterator,
     test_iter: Iterator,
     num_epochs: int,
     learning_rate: float,
+    init_weight: Literal["ch3", "ch6"],
+    force_cpu=False,
 ):
-    def init_weights(m: nn.Module):
-        if type(m) == nn.Linear:
-            nn.init.normal_(m.weight, 0, 0.01)
+    init_method = None  # choose init_weight method
+    if init_weight == "ch3":
+        init_method = init_weights_ch3
+    elif init_weight == "ch6":
+        init_method = init_weights_ch6
+    # use m1 gpu if existed
+    use_mps = not force_cpu and torch.has_mps
 
-    net.apply(init_weights)
+    if use_mps:
+        net = net.to("mps")
+    net.apply(init_method)
     loss = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), learning_rate)
 
@@ -79,6 +97,9 @@ def train_ch3(
     for epoch in range(num_epochs):
         metric = Accumulator(3)  # loss and accuracy respected to the *train* dataset
         for X, y in train_iter:
+            if use_mps:
+                X, y = X.to("mps"), y.to("mps")
+
             optimizer.zero_grad()
             y_hat = net(X)
             l: torch.Tensor = loss(y_hat, y)
@@ -96,6 +117,8 @@ def train_ch3(
     net.eval()
     with torch.no_grad():
         for X, y in test_iter:
+            if use_mps:
+                X, y = X.to("mps"), y.to("mps")
             metric.add(count_correct(net(X), y), y.numel())
 
     test_acc = metric[0] / metric[1]
@@ -111,46 +134,3 @@ def corr2d(X: torch.Tensor, K: torch.Tensor):
             Y[i, j] = (X[i : i + h, j : j + w] * K).sum()
 
     return Y
-
-
-def train_ch6(  # no gpu yet
-    net: nn.Sequential,
-    train_iter: Iterator,
-    test_iter: Iterator,
-    num_epochs: int,
-    learning_rate: float,
-):
-    def init_weights(m: nn.Module):
-        type_module = type(m)
-        if type_module == nn.Linear or type_module == nn.Conv2d:
-            nn.init.xavier_uniform_(m.weight)
-
-    net.apply(init_weights)
-    loss = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(net.parameters(), learning_rate)
-
-    net.train()
-    for epoch in range(num_epochs):
-        metric = Accumulator(3)  # loss and accuracy respected to the *train* dataset
-        for X, y in train_iter:
-            optimizer.zero_grad()
-            y_hat = net(X)
-            l: torch.Tensor = loss(y_hat, y)
-            l.backward()  # *need* to do mean() if reduction was set to 'none'
-            optimizer.step()
-            metric.add(l.item(), count_correct(y_hat, y), y.numel())
-
-        train_loss = metric[0] / metric[2]
-        train_acc = metric[1] / metric[2]
-        print(
-            f"epoch {epoch + 1}: loss {train_loss:.8f}, accuracy {100 * train_acc:.2f}%"
-        )
-
-    metric = Accumulator(2)  # loss and accuracy respected to the *test* dataset
-    net.eval()
-    with torch.no_grad():
-        for X, y in test_iter:
-            metric.add(count_correct(net(X), y), y.numel())
-
-    test_acc = metric[0] / metric[1]
-    print(f"\ntest accuracy: {100 * test_acc:.2f}%")
